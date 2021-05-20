e6997a29dc109222f8c65b7be0c2481dc49a7341
sinkingtitanic <elhoussinetalab@gmail.com>
2021-05-20 12:23:59 +0400

    fixed file logging bug

--- qlogging/__init__.py
+++ qlogging/__init__.py
@@ -1,3 +1,3 @@
 from .qlogging import get_logger, config
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 
--- qlogging/qlogging.py
+++ qlogging/qlogging.py
@@ -7,10 +7,13 @@ from typing import Optional, Dict
 
 from colorama import Fore, Back, Style, init
 
+
 class ColoredFormatter(logging.Formatter):
     """Colored log formatter."""
 
-    def __init__(self, *args, colors: Optional[Dict[str, str]]=None, **kwargs) -> None:
+    def __init__(
+        self, *args, colors: Optional[Dict[str, str]] = None, **kwargs
+    ) -> None:
         """Initialize the formatter with specified format strings."""
 
         super().__init__(*args, **kwargs)
@@ -20,37 +23,45 @@ class ColoredFormatter(logging.Formatter):
     def format(self, record) -> str:
         """Format the specified record as text."""
 
-        record.color = self.colors.get(record.levelname, '')
+        record.color = self.colors.get(record.levelname, "")
         record.reset = Style.RESET_ALL
 
         return super().format(record)
-    
-    
-def get_logger(level='info', logfile=None, logfilemode='a', 
-               loggingmode="short", format_str=None, format_date=None, colors=None, logger_config=None): 
+
+
+def get_logger(
+    level="info",
+    logfile=None,
+    logfilemode="a",
+    loggingmode="short",
+    format_str=None,
+    format_date=None,
+    colors=None,
+    logger_config=None,
+):
     """
     returns Python logging based logger formatted with colors
 
-    :param level: (DEFAULT='info') str of logging level, each str option is mapped to Python logging levels, str options: 
+    :param level: (DEFAULT='info') str of logging level, each str option is mapped to Python logging levels, str options:
                         'info': logging.INFO,
-                        'debug': logging.DEBUG, 
-                        'warning': logging.WARNING, 
-                        'error': logging.ERROR, 
+                        'debug': logging.DEBUG,
+                        'warning': logging.WARNING,
+                        'error': logging.ERROR,
                         'critical': logging.CRITICAL,
                         'notset': logging.NOTSET
     :param logfile: (DEFAULT=None) str path where to save log file, example: '/tmp/my_log.log'
-    :param logfilemode: (DEFAULT='a') str of log file writing mode, same as the ones documented at Python logging package. options: 
-                        'a': appends to logfile 
-                        'w': overwrites logfile 
-    :param loggingmode: (DEFAULT='short') str logging mode to be selected. options: 
+    :param logfilemode: (DEFAULT='a') str of log file writing mode, same as the ones documented at Python logging package. options:
+                        'a': appends to logfile
+                        'w': overwrites logfile
+    :param loggingmode: (DEFAULT='short') str logging mode to be selected. options:
                         'short': will use short str format ('{color}{asctime} {funcName},{lineno}| {message} {reset}') and short date format ('%H:%M:%S')
                         'medium': will use long str format ('color}{asctime} | {filename:8} | {funcName},{lineno} | {message}{reset}') and long date format ('%Y-%m-%d %H:%M:%S')
                         'manual': you need to set :param format_str: and :param format_date: yourself
-    :param format_str: (DEFAULT=None) str of format logging string, only set this if you selected :param loggingmode: as 'manual'. example: 
+    :param format_str: (DEFAULT=None) str of format logging string, only set this if you selected :param loggingmode: as 'manual'. example:
                         'color}{asctime} | {filename:8} | {funcName},{lineno} | {message}{reset}'
-    :param date_str: (DEFAULT=None) str of date logging string, only set this if you selected :param loggingmode: as 'manual'. example: 
+    :param date_str: (DEFAULT=None) str of date logging string, only set this if you selected :param loggingmode: as 'manual'. example:
                         '%Y-%m-%d %H:%M:%S'
-    :param colors: (DEFAULT=None) dict of color settings, only set this if you selected :param loggingmode: as 'manual'. example: 
+    :param colors: (DEFAULT=None) dict of color settings, only set this if you selected :param loggingmode: as 'manual'. example:
                         {
                             'DEBUG': Fore.CYAN + Style.BRIGHT,
                             'INFO': Fore.GREEN + Style.BRIGHT,
@@ -58,7 +69,7 @@ def get_logger(level='info', logfile=None, logfilemode='a',
                             'ERROR': Fore.RED + Style.BRIGHT,
                             'CRITICAL': Fore.RED + Back.WHITE + Style.BRIGHT,
                         }
-    :param logger_config: (DEFAULT=None) dict python logger config if you want to fully overwrite configs. example: 
+    :param logger_config: (DEFAULT=None) dict python logger config if you want to fully overwrite configs. example:
                         {
                             "version": 1,
                             "disable_existing_loggers": False,
@@ -93,69 +104,90 @@ def get_logger(level='info', logfile=None, logfilemode='a',
                             },
                         }
     :return: formated Python logging instance
-    """ 
-    
-    if loggingmode == "short": 
-        format_str = '{color}{asctime} {funcName},{lineno}| {message} {reset}'
-        format_date = '%H:%M:%S'
+    """
+    file_format_str = ""
+    if loggingmode == "short":
+        format_str = "{color}{asctime} {funcName},{lineno}| {message}"
+        file_format_str = "{asctime} {funcName},{lineno}| {message}"
+        format_date = "%H:%M:%S"
         colors = {
-                'DEBUG': Fore.CYAN + Style.BRIGHT,
-                'INFO': Fore.GREEN + Style.BRIGHT,
-                'WARNING': Fore.YELLOW + Style.BRIGHT,
-                'ERROR': Fore.RED + Style.BRIGHT,
-                'CRITICAL': Fore.RED + Back.WHITE + Style.BRIGHT,
-            }
-    elif loggingmode == "long": 
-        format_str = '{color}{asctime} | {filename:8} | {funcName},{lineno} | {message}{reset}'
-        format_date = '%Y-%m-%d %H:%M:%S'
+            "DEBUG": Fore.CYAN + Style.BRIGHT,
+            "INFO": Fore.GREEN + Style.BRIGHT,
+            "WARNING": Fore.YELLOW + Style.BRIGHT,
+            "ERROR": Fore.RED + Style.BRIGHT,
+            "CRITICAL": Fore.RED + Back.WHITE + Style.BRIGHT,
+        }
+    elif loggingmode == "long":
+        format_str = (
+            "{color}{asctime} | {filename:8} | {funcName},{lineno} | {message}"
+        )
+        file_format_str = (
+            "{asctime} | {filename:8} | {funcName},{lineno} | {message}"
+        )
+        format_date = "%Y-%m-%d %H:%M:%S"
         colors = {
-                'DEBUG': Fore.CYAN + Style.BRIGHT,
-                'INFO': Fore.GREEN + Style.BRIGHT,
-                'WARNING': Fore.YELLOW + Style.BRIGHT,
-                'ERROR': Fore.RED + Style.BRIGHT,
-                'CRITICAL': Fore.RED + Back.WHITE + Style.BRIGHT,
-            }
-    elif loggingmode == "manual": 
-        pass 
-    else: 
-        print(" + Error, unknown logging mode {}, please choose: short/long/manual".format(loggingmode))
-    
-    # reseting current logger config 
+            "DEBUG": Fore.CYAN + Style.BRIGHT,
+            "INFO": Fore.GREEN + Style.BRIGHT,
+            "WARNING": Fore.YELLOW + Style.BRIGHT,
+            "ERROR": Fore.RED + Style.BRIGHT,
+            "CRITICAL": Fore.RED + Back.WHITE + Style.BRIGHT,
+        }
+    elif loggingmode == "manual":
+        pass
+    else:
+        print(
+            " + Error, unknown logging mode {}, please choose: short/long/manual".format(
+                loggingmode
+            )
+        )
+
+    # reseting current logger config
     logging.handlers = []
-    
-    formatter = ColoredFormatter(format_str,style='{', datefmt=format_date,colors=colors)
+
+    formatter = ColoredFormatter(
+        format_str, style="{", datefmt=format_date, colors=colors
+    )
+    file_formatter = ColoredFormatter(
+        file_format_str, style="{", datefmt=format_date, colors=colors
+    )
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
-    
+
     logger = logging.getLogger()
     logger.handlers[:] = []
-    logger.addHandler(handler)
-    if logfile != None: 
-        file = logging.FileHandler(logfile,mode=logfilemode)
+    if logfile != None:
+        file = logging.FileHandler(logfile, mode=logfilemode)
+        file.setFormatter(file_formatter)
         logger.addHandler(file)
-    
-    levels = { 
-        'info': logging.INFO,
-        'debug': logging.DEBUG, 
-        'warning': logging.WARNING, 
-        'error': logging.ERROR, 
-        'critical': logging.CRITICAL,
-        'notset': logging.NOTSET
+    logger.addHandler(handler)
+
+    levels = {
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
+        "notset": logging.NOTSET,
     }
-    
+
     logger.setLevel(levels.get(level))
-    try: 
-        if logger_config != None: 
-            for k, v in logger_config.get('formatters').items():
-                if logger_config.get('formatters').get(k).get('format'): 
-                    if not "color" in logger_config.get('formatters').get(k).get('format') : 
-                        current_format = logger_config.get('formatters').get(k).get('format')
-                        logger_config['formatters'][k]['format'] = "%(color)s"+current_format
-                
+    try:
+        if logger_config != None:
+            for k, v in logger_config.get("formatters").items():
+                if logger_config.get("formatters").get(k).get("format"):
+                    if not "color" in logger_config.get("formatters").get(k).get(
+                        "format"
+                    ):
+                        current_format = (
+                            logger_config.get("formatters").get(k).get("format")
+                        )
+                        logger_config["formatters"][k]["format"] = (
+                            "%(color)s" + current_format
+                        )
+
             config.dictConfig(logger_config)
-        os.system("cls" or "clear") # for windows support 
-    except: 
+        os.system("cls" or "clear")  # for windows support
+    except:
         pass
 
     return logger
-
